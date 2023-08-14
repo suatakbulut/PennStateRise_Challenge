8bd264aeea99cc322556caf7667932755ee3062b
suatakbulut <akbulut.suat@gmail.com>
2023-08-14 18:11:11 -0400

    post_analaysis permutation_importance_top10 n_jobs 2 >> 20

--- code_workspace/post_analysis.py
+++ code_workspace/post_analysis.py
@@ -1,30 +1,38 @@
-from sklearn.inspection import permutation_importance
-from utils_model import * 
 import matplotlib.pyplot as plt
-import seaborn as sns 
+import seaborn as sns
+import statsmodels.api as sm
+from sklearn.inspection import permutation_importance
+
+from utils_model import *
+
 
 def permutation_importance_top10(model, X_test, y_test):
     start = time.time()
-    result = permutation_importance(model, X_test, y_test, n_repeats=2, random_state=0, n_jobs=-1)
+    result = permutation_importance(
+        model, X_test, y_test, n_repeats=20, random_state=0, n_jobs=-1)
     imp_mean = pd.Series(result.importances_mean, index=X_test.columns)
     imp = pd.DataFrame(result.importances, index=X_test.columns)
 
-    perm_importance_labels = list(imp_mean.sort_values(ascending=False).index[:10])
+    perm_importance_labels = list(
+        imp_mean.sort_values(ascending=False).index[:10])
     perm_importance_labels.reverse()
 
     perm_importance_values = imp.loc[perm_importance_labels]
 
     end = time.time()
     elapsed_time = (end-start)/60
-    print(f"Elapsed time to compute permutation importances: {elapsed_time:.2f} mins")
+    print(
+        f"Elapsed time to compute permutation importances: {elapsed_time:.2f} mins")
     return [perm_importance_values, perm_importance_labels]
 
 
 def feature_importance_top10(model, X_test, y_test):
     start = time.time()
     predictor = model.named_steps["predictor"]
-    feature_importance = pd.Series(predictor.feature_importances_, index=X_test.columns)
-    top10_feature_importance = feature_importance.sort_values(ascending=False)[:10]
+    feature_importance = pd.Series(
+        predictor.feature_importances_, index=X_test.columns)
+    top10_feature_importance = feature_importance.sort_values(ascending=False)[
+        :10]
     feature_importance_values = top10_feature_importance.values
     feature_importance_labels = top10_feature_importance.index
 
@@ -43,7 +51,8 @@ def feature_importance_top10(model, X_test, y_test):
 
     end = time.time()
     elapsed_time = (end - start) / 60
-    print(f"Elapsed time to compute feature importances: {elapsed_time:.2f} mins")
+    print(
+        f"Elapsed time to compute feature importances: {elapsed_time:.2f} mins")
     return [
         feature_importance_values,
         top10_feature_importance_std,
@@ -53,16 +62,20 @@ def feature_importance_top10(model, X_test, y_test):
 
 def plot_perm_and_feature_importances(model, X_test, y_test, model_type):
     print("Calculating permutation importance. This could take a while..")
-    perm_importance_values, perm_importance_labels = permutation_importance_top10(model, X_test, y_test)
-    feature_importance_values, top10_feature_importance_std, feature_importance_labels = feature_importance_top10(model, X_test, y_test)
-    
+    perm_importance_values, perm_importance_labels = permutation_importance_top10(
+        model, X_test, y_test)
+    feature_importance_values, top10_feature_importance_std, feature_importance_labels = feature_importance_top10(
+        model, X_test, y_test)
+
     fig, axes = plt.subplots(1, 2, figsize=(16, 6))
-    sns.barplot(x=feature_importance_values, y=feature_importance_labels, ax=axes[0], yerr = top10_feature_importance_std)
+    sns.barplot(x=feature_importance_values, y=feature_importance_labels,
+                ax=axes[0], yerr=top10_feature_importance_std)
     axes[0].set_title("Top 10 Feature Importance")
     axes[0].set_ylabel("Features")
     axes[0].set_xlim([0, 0.25])
 
-    axes[1].boxplot(perm_importance_values.T, vert=False, labels=perm_importance_labels)
+    axes[1].boxplot(perm_importance_values.T, vert=False,
+                    labels=perm_importance_labels)
     axes[1].set_title("Top 10 Permutation Importance")
     axes[1].set_xlim([0, 0.025])
 
@@ -70,4 +83,44 @@ def plot_perm_and_feature_importances(model, X_test, y_test, model_type):
     fig_path = f"../data/{model_type}_permutation_feature_importance.png"
     plt.savefig(fig_path)
     print(f"Figure is saved at {fig_path}")
-    return fig 
+    return fig
+
+
+def calculate_stats_logit_metrics(logit, X_test, y_test):
+    yhat = logit.predict(sm.add_constant(X_test))
+    y_pred = list(map(round, yhat))
+
+    auc_score = roc_auc_score(y_test, yhat)
+    f_score = f1_score(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    return auc_score, f_score, accuracy, precision, recall
+
+
+def print_logit_results(logit, X_train, X_test, y_train, y_test, imp_features):
+    auc_score, f_score, accuracy, precision, recall = calculate_stats_logit_metrics(
+        logit, X_test, y_test
+    )
+    (
+        auc_score_tr,
+        f_score_tr,
+        accuracy_tr,
+        precision_tr,
+        recall_tr,
+    ) = calculate_stats_logit_metrics(logit, X_train, y_train)
+
+    labels = ["const"]
+    for label in imp_features:
+        labels.append(label)
+    
+    print(logit.summary(xname=labels))
+
+    print("\n\n===========================")
+    print("== Test Set vs Train Set ==")
+    print(f"AUC Score : {auc_score:.4f} | {auc_score_tr:.4f}")
+    print(f"Recall    : {recall:.4f} | {recall_tr:.4f}")
+    print(f"F1 Score  : {f_score:.4f} | {f_score_tr:.4f}")
+    print(f"Accuracy  : {accuracy:.4f} | {accuracy_tr:.4f}")
+    print(f"Precision : {precision:.4f} | {precision_tr:.4f}")
+    print("===========================\n")
